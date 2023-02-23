3dba7ef21f78b2af599116748a9914bd8d52bfca
Juan Fox <juannfox@hotmail.com>
2023-02-22 23:37:53 -0300

    Dataset download

--- __init__.py
+++ __init__.py
@@ -28,6 +28,11 @@ class FileName():
         else:
             self.name = search.group(1)
             self.extension = search.group(3)
+    
+    def __str__(self):
+        ext = "" if self.extension is None else f".{self.extension}"
+        return f"{self.name}{ext}"
+
 
 @dataclass
 class HttpClient:
@@ -63,6 +68,7 @@ class HttpClient:
             response = None
         return response
 
+
 class DatasetResource():
     """
     A resource within the gov. API response.
@@ -72,21 +78,52 @@ class DatasetResource():
     url: str
     file_name: FileName
 
+    def __init__(self, data: dict):
+        try:
+            self.name = data["name"]
+            self.access_url = data["accessURL"]
+            self.url = data["url"]
+        except KeyError as exc:
+            raise ValueError("Wrong data resource format.") from exc
+        try:
+            self.file_name = FileName(data["fileName"])
+        except KeyError:
+            self.file_name = None
+
+    def __str__(self):
+        return f"name: {self.name}\n" \
+            + f"access_url: {self.access_url}\n" \
+            + f"url: {self.url}\n" \
+            + f"file_name: {self.file_name}"
+
+
 class DatasetResponse():
     """
     A response from the Argentina's gov. API.
     """
     success: bool
     id: str
-    resources: object
+    resources: None
 
     def __init__(self, data: dict):
         try:
             self.success = data["success"]
-            self.id = data["id"]
-            self.resources = data["result"]["resources"]
-        except KeyError:
-            raise ValueError("Wrong dataset format.")
+            self.id = data["result"]["id"]
+            resources = data["result"]["resources"]
+            self.resources = []
+            for resource in resources:
+                self.resources.append(DatasetResource(resource))
+        except KeyError as exc:
+            raise ValueError("Wrong dataset format.") from exc
+
+    def __str__(self):
+        res = ""
+        for resource in self.resources:
+            res += f"---\n{resource.__str__()}\n"
+        return f"success: {self.success}\n" \
+            + f"id: {self.id}\n" \
+            + f"resources: \n{res}"
+
 
 @dataclass
 class APIGobierno:
@@ -116,4 +153,4 @@ class APIGobierno:
 
 
 gov = APIGobierno()
-print(gov.get_dataset_metadata().success)
+print(gov.get_dataset_metadata())
